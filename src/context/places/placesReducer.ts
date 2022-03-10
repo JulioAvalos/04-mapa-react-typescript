913515c83c69a01ff9ab3fa10f3f3971b0ec2b99
JulioAvalos <julioavalos2@msn.com>
2022-03-09 18:31:51 -0600

    Definiendo estructura inicial del proyecto

---
+++ src/context/places/placesReducer.ts
@@ -0,0 +1,10 @@
+import { PlacesState } from './PlacesProvider';
+
+type PlacesAction = {
+    type: 'setUserLocation',
+    payload: [number, number]
+};
+
+export const placesReducer = (state: PlacesState, action: PlacesAction ): PlacesState => {
+    
+}
